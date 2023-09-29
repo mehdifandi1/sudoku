89305c68788e0c4fc7eff3dd18dc0ba2cc6f17ab
root <root@Julien_Portable.localdomain>
2023-09-29 10:32:15 +0200

    new main

--- main.go
+++ main.go
@@ -2,19 +2,35 @@ package main
 
 import (
 	rl "github.com/gen2brain/raylib-go/raylib"
+	//"fmt"
+	"time"
+	
 )
+
+
+
+
+
+
+
 
 func main() {
 	rl.InitWindow(800, 450, "raylib [core] example - basic window")
 	defer rl.CloseWindow()
-	rl.SetTargetFPS(60)
-	game()
+	rl.SetTargetFPS(240)
+
+
+	display_text_Anim()
+	time.Sleep(1 * time.Second)
+	menu()
+	time.Sleep(1 * time.Second)
+
+
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
-
 		rl.ClearBackground(rl.RayWhite)
-
+		rl.DrawFPS(0,0)
 		rl.EndDrawing()
 	}
 }
